c84e94bc83845f9720da9529329edb6147a46014
Filip Chalupa <chalupa.filip@gmail.com>
2015-03-18 01:12:14 +0100

    Process incoming except FOTO

--- robot.js
+++ robot.js
@@ -4,9 +4,9 @@ var Robot = function(socket) {
     this.name = '';
     this.password = '';
     this.timeout = false;
-    this.inputBuffer = '';
-    this.processBuffer = '';
-    this.seprarator = '\r\n';
+
+    this.state = 'login';
+    this.fotoLength;
 
     this.log('Robot connected.');
 
@@ -48,10 +48,80 @@ Robot.prototype.addListeners = function() {
         that.log('Robot disconnected.');
         clearTimeout(that.timeout);
     });
+    this.processData();
+};
+
+Robot.prototype.processData = function() {
+    var that = this;
+    var input = '';
+    var separator = {
+        'login': '\r\n',
+        'password': '\r\n',
+        'message': ' ',
+        'info': '\r\n',
+        'fotoLength': ' '
+    };
+
     this.socket.on('data', function(data) {
-        this.inputBuffer += data.toString();
-        that.read();
+        var splitedData;
+        input += data.toString();
+
+        if (that.state === 'foto') {
+            // TODO
+        } else {
+            while (true) {
+                splitedData = input.split(separator[that.state]);
+                if (splitedData.length === 1) {
+                    break;
+                }
+                that.processInputPart(splitedData[0]);
+                input = splitedData[1];
+            }
+        }
     });
+};
+
+Robot.prototype.processInputPart = function(data) {
+    switch (this.state) {
+        case 'login':
+            this.name = data;
+            this.log('Waiting for password.');
+            this.sendMessage('PASSWORD');
+            this.state = 'password';
+            break;
+        case 'password':
+            this.password = data;
+            if (this.validateCredentials() === true) {
+                this.sendMessage('OK');
+                this.log('Waiting for message.');
+                this.state = 'message';
+            }
+            break;
+        case 'message':
+            if (data === 'INFO') {
+                this.state = 'info';
+            } else if (data === 'FOTO') {
+                this.state = 'fotoLength';
+            } else {
+                this.log('Syntax error.');
+                this.closeConnection('SYNTAX_ERROR');
+            }
+            break;
+        case 'info':
+            this.log('INFO: ' + data);
+            this.state = 'message';
+            break;
+        case 'fotoLength':
+            this.fotoLength = parseInt(data, 10);
+            if (isNaN(this.fotoLength)) {
+                this.log('Syntax error.');
+                this.closeConnection('SYNTAX_ERROR');
+            }
+            this.state = 'foto';
+            break;
+        default:
+            this.log('Something went wrong.');
+    }
 };
 
 Robot.prototype.setConnectionTimeout = function() {
@@ -76,83 +146,12 @@ Robot.prototype.validateCredentials = function() {
 
     if (parseInt(this.password, 10) === sum && this.name.substr(0, 5) === 'Robot') {
         this.log('Robot verified.');
+        return true;
     } else {
         this.log('Wrong credentials.');
         this.closeConnection('LOGIN_FAILED');
     }
+    return false;
 };
-
-Robot.prototype.read = function() {
-    this.processBuffer += this.dataBuffer[0];
-    this.dataBuffer = this.dataBuffer.substr(1);
-
-    if (this.seprarator !== false) {
-        if (this.processBuffer.substr(-2) === this.separator) {
-
-        }
-    }
-
-
-    if (this.inputBuffer.length > 0) {
-        this.read();
-    }
-};
-
-/*
-
-Robot.prototype.loginManager = function(data) {
-    var length = data.length;
-    for (var i = 0; i < length; i++) {
-        if (data[i] === '\n' && this.name.slice(-1) === '\r') {
-            this.name = this.name.substr(0, this.name.length - 1);
-            this.log('Waiting for password.');
-            this.sendMessage('PASSWORD');
-            this.incomingDataManager = this.passwordManager;
-            this.incomingDataManager(data.substr(i + 1));
-            break;
-        } else {
-            this.name += data[i];
-        }
-    }
-};
-
-Robot.prototype.passwordManager = function(data) {
-    var length = data.length;
-    for (var i = 0; i < length; i++) {
-        if (data[i] === '\n' && this.password.slice(-1) === '\r') {
-            this.password = this.password.substr(0, this.password.length - 1);
-            this.validateCredentials();
-            this.sendMessage('OK');
-            this.incomingDataManager = this.messageTypeManager;
-            this.incomingDataManager(data.substr(i + 1));
-            break;
-        } else {
-            this.password += data[i];
-        }
-    }
-};
-
-Robot.prototype.messageTypeManager = function(data) {
-    console.log('Reading message type');
-    //this.incomingDataManager(data);
-};
-
-Robot.prototype.infoManager = function(data) {
-    console.log('Reading info');
-    //this.incomingDataManager(data);
-};
-
-Robot.prototype.fileManager = function(data) {
-    console.log('Reading file');
-    //this.incomingDataManager(data);
-};
-*/
 
 module.exports = Robot;
-
-
-
-
-
-
-
